49168abe94303843483e02ac5c92619a548e4ec1
Rajesh <149164814+RJKN13@users.noreply.github.com>
2024-08-15 15:04:32 +0200

    Added endpoint to find the most repeated word in the text.

--- server/server.js
+++ server/server.js
@@ -10,3 +10,35 @@ server.use(express.json())
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
+
+server.post('/api/process-text', async (req, res) => {
+  const uploadedFileContent = req.body.text
+  
+        // change the text to lowercase & replace special characters
+        const words = uploadedFileContent
+            .toLowerCase()           
+            .replace(/[^\w\ ]/g, '') 
+            .split(/\ +/);           
+
+        // word count for every occurrence
+        const wordCount = {};
+        for (const word of words) {
+            if (word) { 
+                wordCount[word] = (wordCount[word] || 0) + 1;
+            }
+        }
+
+        // find the most repeated word
+        let maxCount = 0;
+        let mostRepeatedWord = '';
+        for (const word in wordCount) {
+            if (wordCount[word] > maxCount) {
+                maxCount = wordCount[word];
+                mostRepeatedWord = word;
+            }
+        }
+        
+        // TO-DO: append and replace the text 
+  
+  res.json(mostRepeatedWord)   
+  })
